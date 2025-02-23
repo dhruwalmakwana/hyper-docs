98983220d2dd0d43e2e118b8bab297721cd7b100
Dhruwal Makwana <dhruwalmakwana@gmail.com>
2025-02-23 17:06:53 +0530

    wip:search component in the home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Navbar } from "./navbar";
+
+const Home = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
+        <Navbar />
+      </div>
+      Click <Link href="/documents/123"><span className="text-blue-500 underline">&nbsp;here&nbsp;</span></Link>
+      to go to document id
+    </div>
+  );
+}
+
+export default Home;
