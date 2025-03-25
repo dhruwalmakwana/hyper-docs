c257e8bbb00d68e4604c4901b212000fb9fa93ae
Dhruwal Makwana <dhruwalmakwana@gmail.com>
2025-03-26 04:48:19 +0530

    hotfix: fixed error in authentication and document retrival

--- convex/schema.ts
+++ convex/schema.ts
@@ -9,10 +9,11 @@ export default defineSchema({
         roomId: v.optional(v.string()),
         organizationId: v.optional(v.string())
     })
-    .index("by_owner_id", ['ownerId'])
-    .index("by_organization_id", ['organizationId'])
-    .searchIndex("search_title", {
-        searchField: "title",
-        filterFields: ["ownerId", "organizationId"],
-    }),
+        .index("by_owner_id", ['ownerId'])
+        .index("by_owner_and_org", ['ownerId', 'organizationId'])
+        .index("by_organization_id", ['organizationId'])
+        .searchIndex("search_title", {
+            searchField: "title",
+            filterFields: ["ownerId", "organizationId"],
+        }),
 });
